66883550d02abdf78ecf71f99a0e7634163cfc3f
Van Simmons <rvsrvs@gmail.com>
2020-04-06 17:21:49 -0400

    Initial Commit

---
+++ FreeCombine/FreeCombine.h
@@ -0,0 +1,19 @@
+//
+//  FreeCombine.h
+//  FreeCombine
+//
+//  Created by Van Simmons on 4/6/20.
+//  Copyright © 2020 ComputeCycles, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FreeCombine.
+FOUNDATION_EXPORT double FreeCombineVersionNumber;
+
+//! Project version string for FreeCombine.
+FOUNDATION_EXPORT const unsigned char FreeCombineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FreeCombine/PublicHeader.h>
+
+
