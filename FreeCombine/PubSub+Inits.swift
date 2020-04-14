152a9875e6b50cd203e051ffecd805db2355d742
Van Simmons <rvsrvs@gmail.com>
2020-04-14 13:38:46 -0400

    fix init

--- FreeCombine/PubSub+Inits.swift
+++ FreeCombine/PubSub+Inits.swift
@@ -68,17 +68,18 @@ extension Subscription {
     }
 }
 
-//extension Composer {
-//    init(
-//        input: @escaping ((Output) -> Demand) -> (Input) -> Demand,
-//        completion: @escaping ((Completion<OutputFailure>) -> Void) -> (Completion<InputFailure>) -> Void,
-//        request: @escaping ((Demand) -> Void) -> (Demand) -> Void,
-//        control: @escaping ((Control<OutputControl>) -> Void) -> (Control<InputControl>) -> Void
-//    ) {
-//        self.composition = Self.Composition.publisherSubscriber(
-//            Subscriber<Output, OutputFailure>.transform(input, completion),
-//            receive,
-//            Subscription<OutputControl>.transform(request, control)
-//        )
-//    }
-//}
+extension Composer {
+    init(
+        input: @escaping ((Output) -> Demand) -> (Input) -> Demand,
+        completion: @escaping ((Completion<OutputFailure>) -> Void) -> (Completion<InputFailure>) -> Void,
+        subscribe: @escaping (Subscriber<Output, OutputFailure>) -> Subscription<OutputControl>,
+        request: @escaping ((Demand) -> Void) -> (Demand) -> Void,
+        control: @escaping ((Control<OutputControl>) -> Void) -> (Control<InputControl>) -> Void
+    ) {
+        self.composition = Self.Composition.publisherSubscriber(
+            recast(Subscriber<Output, OutputFailure>.transform(input, completion)),
+            recast(subscribe),
+            recast(Subscription<OutputControl>.transform(request, control))
+        )
+    }
+}
