6fd943c839fe3f0785da754415393d1742eea744
Van Simmons <rvsrvs@gmail.com>
2020-04-14 22:06:00 -0400

    checkpoint

--- FreeCombine/Composition.swift
+++ FreeCombine/Composition.swift
@@ -6,6 +6,15 @@
 //  Copyright © 2020 ComputeCycles, LLC. All rights reserved.
 //
 
+// FlatMaps
+//extension Publication {
+//    static func flatMap<T>(
+//        _ transform: @escaping (Input) -> Producer<T, OutputFailure>
+//    ) -> (Self) -> Publication<Output, OutputControl, OutputFailure, T, OutputControl, OutputFailure> {
+//
+//    }
+//}
+
 // Maps
 extension Publication {
     func map<T>(
@@ -13,6 +22,16 @@ extension Publication {
     ) -> Publication<Output, OutputControl, OutputFailure, T, OutputControl, OutputFailure> {
         Publication<Output, OutputControl, OutputFailure, T, OutputControl, OutputFailure>(
             hoist: recast(Subscriber<Output, OutputFailure>.map(transform)),
+            subscribe: receive,
+            lower: identity
+        )
+    }
+
+    func contraMap(
+        _ transform: @escaping (Demand) -> Demand
+    ) -> Publication<Output, OutputControl, OutputFailure, Output, OutputControl, OutputFailure> {
+        Publication<Output, OutputControl, OutputFailure, Output, OutputControl, OutputFailure>(
+            hoist: Subscriber<Output, OutputFailure>.contraMap(transform),
             subscribe: receive,
             lower: identity
         )
--- FreeCombine/Subscriber+Transforms.swift
+++ FreeCombine/Subscriber+Transforms.swift
@@ -8,14 +8,14 @@
 
 // Maps
 extension Subscriber {
-    static func map<UpstreamInput>(
-        _ transform: @escaping (Input) -> UpstreamInput
-    ) -> (Self) -> Subscriber<UpstreamInput, Failure> {
+    static func map<DownstreamInput>(
+        _ transform: @escaping (Input) -> DownstreamInput
+    ) -> (Self) -> Subscriber<DownstreamInput, Failure> {
         { subscriber in
-            recast(Subscriber(
-                input: transform >>> recast(subscriber.input),
+            Subscriber<DownstreamInput, Failure>(
+                input: recast(transform) >>> subscriber.input,
                 completion: subscriber.completion
-            ))
+            )
         }
     }
 
@@ -23,20 +23,21 @@ extension Subscriber {
         _ transform: @escaping (Demand) -> Demand
     ) -> (Self) -> Subscriber<Input, Failure> {
         { subscriber in
-            recast(Subscriber(
+            Subscriber<Input, Failure>(
                 input: subscriber.input >>> transform,
                 completion: subscriber.completion
-            ))
+            )
         }
     }
-    static func mapError<UpstreamFailure: Error>(
-        _ transform: @escaping (Failure) -> UpstreamFailure
-    ) -> (Self) -> Subscriber<Input, UpstreamFailure> {
+    
+    static func mapError<DownstreamFailure: Error>(
+        _ transform: @escaping (Failure) -> DownstreamFailure
+    ) -> (Self) -> Subscriber<Input, DownstreamFailure> {
         { subscription in
-            recast(Subscriber(
+            Subscriber<Input, DownstreamFailure>(
                 input: subscription.input,
                 completion: recast(transform) >>> subscription.completion
-            ))
+            )
         }
     }
 }
