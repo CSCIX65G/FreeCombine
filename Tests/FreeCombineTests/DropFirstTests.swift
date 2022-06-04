e35a7bab67f312c3f10fbba03f39bf3d18eaa6af
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-06-04 09:04:42 -0400

    Add DropFirst
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

---
+++ Tests/FreeCombineTests/DropFirstTests.swift
@@ -0,0 +1,60 @@
+//
+//  DropFirstTests.swift
+//  
+//
+//  Created by Van Simmons on 6/4/22.
+//
+
+import XCTest
+@testable import FreeCombine
+
+class DropFirstTests: XCTestCase {
+
+    override func setUpWithError() throws { }
+
+    override func tearDownWithError() throws { }
+
+    func testSimpleDropFirst() async throws {
+        let expectation1 = await CheckedExpectation<Void>()
+        let unfolded = (0 ..< 100).asyncPublisher
+
+        let counter1 = Counter()
+        let u1 = await unfolded
+            .dropFirst(27)
+            .sink { (result: AsyncStream<Int>.Result) in
+            switch result {
+                case .value:
+                    await counter1.increment()
+                    return .more
+                case let .completion(.failure(error)):
+                    XCTFail("Got an error? \(error)")
+                    return .done
+                case .completion(.finished):
+                    let count = await counter1.count
+                    guard count == 73 else {
+                        XCTFail("Incorrect count: \(count) in subscription 1")
+                        return .done
+                    }
+                    do {
+                        try await expectation1.complete()
+                    }
+                    catch { XCTFail("Failed to complete with error: \(error)") }
+                    return .done
+                case .completion(.cancelled):
+                    XCTFail("Should not have cancelled")
+                    return .done
+            }
+        }
+        do {
+            try await FreeCombine.wait(for: expectation1, timeout: 100_000_000)
+        } catch {
+            XCTFail("Timed out")
+        }
+        do {
+            let finalValue = try await u1.value
+            XCTAssert(finalValue == .done, "Did not complete")
+        } catch {
+            XCTFail("Failed to get final value")
+        }
+    }
+}
