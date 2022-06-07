436870754d0fa22b66a5ae5a78bd371f5d168c9d
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-06-07 11:43:24 -0400

    Remove Combinator protocol
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

--- Sources/FreeCombine/Publishers/Merged.swift
+++ Sources/FreeCombine/Publishers/Merged.swift
@@ -54,6 +54,7 @@ public func merge<Output>(
                 }
             },
             reducer: MergeState<Output>.reduce
-        )
+        ),
+        extractor: \.mostRecentDemand
     )
 }
