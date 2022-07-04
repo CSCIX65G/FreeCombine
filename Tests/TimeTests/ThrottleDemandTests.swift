2ace6e04b5086c9994a7501d4b7ac41567be7b75
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-07-04 14:43:57 -0400

    Add throttle demand test
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

--- Tests/TimeTests/ThrottleDemandTests.swift
+++ Tests/TimeTests/ThrottleDemandTests.swift
@@ -15,6 +15,42 @@ class ThrottleDemandTests: XCTestCase {
 
     override func tearDownWithError() throws { }
 
-    func testSimpleThrottle() throws {
+    func testSimpleThrottle() async throws {
+        let expectation = await Expectation<Void>()
+
+        let counter = Counter()
+        let t = await (1 ... 10).asyncPublisher
+            .throttleDemand(interval: .milliseconds(100))
+            .sink({ value in
+                switch value {
+                    case .value(_):
+                        await counter.increment()
+                        return .more
+                    case let .completion(.failure(error)):
+                        XCTFail("Got unexpected failure: \(error)")
+                        return .done
+                    case .completion(.finished):
+                        let count = await counter.count
+                        do { try await expectation.complete() }
+                        catch {
+                            XCTFail("Failed to complete: \(error), count = \(count)")
+                        }
+                        return .done
+                    case .completion(.cancelled):
+                        XCTFail("Should not have cancelled")
+                        return .done
+                }
+        })
+
+        do {
+            try await FreeCombine.wait(for: expectation, timeout: 1_000_000_000)
+            t.cancel()
+        } catch {
+            let count = await counter.count
+            XCTAssert(count < 12, "Got too many = \(count)")
+            XCTAssert(count > 10, "Got too few = \(count)")
+        }
+
+        _ = await t.result
     }
 }
