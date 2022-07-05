7c46388cba3d7eb3ca57424d401a3dc917f81dd6
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-07-05 16:56:09 -0400

    First throttle
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

--- Sources/FreeCombine/Subject/CurrentValueSubject.swift
+++ Sources/FreeCombine/Subject/CurrentValueSubject.swift
@@ -10,21 +10,24 @@ public func CurrentValueSubject<Output>(
     line: UInt = #line,
     deinitBehavior: DeinitBehavior = .assert,
     currentValue: Output,
-    buffering: AsyncStream<DistributorState<Output>.Action>.Continuation.BufferingPolicy = .bufferingOldest(1),
+    buffering: AsyncStream<RepeatReceiveState<Output>.Action>.Continuation.BufferingPolicy = .bufferingOldest(1),
     onStartup: Resumption<Void>
 ) async throws -> Subject<Output> {
-    try await .init(stateTask: Channel.init(buffering: buffering) .stateTask(
-        file: file,
-        line: line,
-        deinitBehavior: deinitBehavior,
-        initialState: { channel in .init(currentValue: currentValue, nextKey: 0, downstreams: [:]) },
-        onStartup: onStartup,
-        reducer: Reducer(
-            onCompletion: DistributorState<Output>.complete,
-            disposer: DistributorState<Output>.dispose,
-            reducer: DistributorState<Output>.reduce
+    try await .init(
+        buffering: buffering,
+        stateTask: Channel.init(buffering: .unbounded) .stateTask(
+            file: file,
+            line: line,
+            deinitBehavior: deinitBehavior,
+            initialState: { channel in .init(currentValue: currentValue, nextKey: 0, downstreams: [:]) },
+            onStartup: onStartup,
+            reducer: Reducer(
+                onCompletion: DistributorState<Output>.complete,
+                disposer: DistributorState<Output>.dispose,
+                reducer: DistributorState<Output>.reduce
+            )
         )
-    ) )
+    )
 }
 
 public func CurrentValueSubject<Output>(
@@ -32,17 +35,20 @@ public func CurrentValueSubject<Output>(
     line: UInt = #line,
     deinitBehavior: DeinitBehavior = .assert,
     currentValue: Output,
-    buffering: AsyncStream<DistributorState<Output>.Action>.Continuation.BufferingPolicy = .bufferingOldest(1)
+    buffering: AsyncStream<RepeatReceiveState<Output>.Action>.Continuation.BufferingPolicy = .bufferingOldest(1)
 ) async throws -> Subject<Output> {
-    try await .init(stateTask: try await Channel(buffering: .unbounded).stateTask(
-        file: file,
-        line: line,
-        deinitBehavior: deinitBehavior,
-        initialState: { channel in .init(currentValue: currentValue, nextKey: 0, downstreams: [:]) },
-        reducer: Reducer(
-            onCompletion: DistributorState<Output>.complete,
-            disposer: DistributorState<Output>.dispose,
-            reducer: DistributorState<Output>.reduce
+    try await .init(
+        buffering: buffering,
+        stateTask: try await Channel(buffering: .unbounded).stateTask(
+            file: file,
+            line: line,
+            deinitBehavior: deinitBehavior,
+            initialState: { channel in .init(currentValue: currentValue, nextKey: 0, downstreams: [:]) },
+            reducer: Reducer(
+                onCompletion: DistributorState<Output>.complete,
+                disposer: DistributorState<Output>.dispose,
+                reducer: DistributorState<Output>.reduce
+            )
         )
-    ) )
+    )
 }
---
+++ Tests/TimeTests/ThrottleTests.swift
@@ -0,0 +1,49 @@
+//
+//  ThrottleTests.swift
+//  
+//
+//  Created by Van Simmons on 7/5/22.
+//
+
+import XCTest
+@testable import FreeCombine
+@testable import Time
+
+class ThrottleTests: XCTestCase {
+
+    override func setUpWithError() throws { }
+
+    override func tearDownWithError() throws { }
+
+    func testSimpleThrottle() async throws {
+        let expectation = await Expectation<Void>()
+
+        let counter = Counter()
+        let t = await (1 ... 15).asyncPublisher
+            .throttle(interval: .milliseconds(1000), latest: false)
+            .sink({ value in
+                switch value {
+                    case .value(_):
+                        await counter.increment()
+                        return .more
+                    case let .completion(.failure(error)):
+                        XCTFail("Got unexpected failure: \(error)")
+                        return .done
+                    case .completion(.finished):
+                        return .done
+                    case .completion(.cancelled):
+                        return .done
+                }
+        })
+
+        do {
+            try await FreeCombine.wait(for: expectation, timeout: 1_000_000_000)
+        } catch {
+            t.cancel()
+            let count = await counter.count
+            XCTAssert(count == 2, "Got wrong count = \(count)")
+        }
+
+        _ = await t.result
+    }
+}
