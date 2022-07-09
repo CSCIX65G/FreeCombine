b9cad152cda6bb8d1289b4029424ef34063df4bd
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-07-09 18:27:55 -0400

    Undo package version change mistake
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
     name: "FreeCombine",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .watchOS(.v8),
-        .tvOS(.v15)
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
+        .tvOS(.v13)
     ],
     products: [
         .library(
