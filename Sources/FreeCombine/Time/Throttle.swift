9e410eda112d6e73e34c4f047986c880dda0a0da
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-07-20 07:53:40 -0400

    checkpointing
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

--- Sources/FreeCombine/Time/Throttle.swift
+++ Sources/FreeCombine/Time/Throttle.swift
@@ -6,6 +6,47 @@
 //
 
 extension Publisher {
+    enum ThrottlerError: Error {
+        case alreadyActivated
+    }
+    actor Throttler {
+        private(set) var subject: Subject<Output>!
+        private(set) var cancellable: Cancellable<Demand>!
+        private(set) var timerCancellable: Cancellable<Demand>!
+
+        var value: AsyncStream<Output>.Result? = .none
+        func set(value: AsyncStream<Output>.Result?) { self.value = value }
+        var completion: AsyncStream<Output>.Result? = .none
+        func set(completion: AsyncStream<Output>.Result?) { self.completion = completion }
+
+        func activate (
+            interval: Duration,
+            latest: Bool = false,
+            downstream: @Sendable @escaping (AsyncStream<Output>.Result) async throws -> Demand
+        ) async throws -> Void {
+            guard subject == nil else { return }
+            subject = try await PassthroughSubject(buffering: .bufferingNewest(1))
+            cancellable = await subject.publisher().sink(downstream)
+            timerCancellable = await Heartbeat(interval: interval).sink { _ in
+                if let value = await self.value {
+                    try await self.subject.send(value)
+                    await self.set(value: .none)
+                }
+                if let completion = await self.completion {
+                    try await self.subject.send(completion)
+                    try await self.cleanup()
+                }
+            }
+        }
+
+        private func cleanup() async throws  -> Void {
+            _ = await timerCancellable.cancelAndAwaitResult()
+            try await subject.finish()
+            _ = await cancellable.result
+            _ = await subject.result
+        }
+    }
+
     private func cleanup(
         _ subject: Subject<Output>,
         _ subjectRef: ValueRef<Subject<Output>?>,
@@ -29,6 +70,7 @@ extension Publisher {
         bufferSize: Int = 1
     ) -> Self {
         .init { continuation, downstream in
+            let throttler: Throttler? = .init()
             let subjectRef = ValueRef<Subject<Output>?>(value: .none)
             let cancellableRef = ValueRef<Cancellable<Demand>?>(value: .none)
             let valueRef = ValueRef<AsyncStream<Output>.Result?>(value: .none)
@@ -37,6 +79,8 @@ extension Publisher {
             return self(onStartup: continuation) { r in
                 var vSubject: Subject<Output>! = await subjectRef.value
                 var vCancellable: Cancellable<Demand>! = await cancellableRef.value
+                try await throttler?.activate(interval: interval, latest: latest, downstream: downstream)
+
                 if vSubject == nil {
                     vSubject = try await PassthroughSubject(buffering: .bufferingNewest(1))
                     await subjectRef.set(value: vSubject)
