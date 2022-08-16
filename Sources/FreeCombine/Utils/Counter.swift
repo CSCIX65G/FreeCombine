bd07c7b20af61c4d98441a745f872ed224083ffd
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-08-16 17:41:11 -0400

    Move Counter to not being an Actor
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

--- Sources/FreeCombine/Utils/Counter.swift
+++ Sources/FreeCombine/Utils/Counter.swift
@@ -4,35 +4,34 @@
 //
 //  Created by Van Simmons on 3/1/22.
 //
+import Atomics
 
-public actor Counter {
-    public private(set) var count = 0
+public struct Counter {
+    private let atomicValue: ManagedAtomic<Int> = .init(0)
 
     public init(count: Int = 0) {
-        self.count = count
+        self.atomicValue.store(count, ordering: .relaxed)
+    }
+
+    public var count: Int {
+        return atomicValue.load(ordering: .sequentiallyConsistent)
     }
 
     @discardableResult
-    public func increment() -> Int {
-        count += 1
-        return count
+    public func increment(by: Int = 1) -> Int {
+        var c = atomicValue.load(ordering: .sequentiallyConsistent)
+        while !atomicValue.compareExchange(expected: c, desired: c + by, ordering: .sequentiallyConsistent).0 {
+            c = atomicValue.load(ordering: .sequentiallyConsistent)
+        }
+        return c + by
     }
 
     @discardableResult
-    public func increment(by: Int) -> Int {
-        count += by
-        return count
-    }
-
-    @discardableResult
-    public func decrement() -> Int {
-        count -= 1
-        return count
-    }
-
-    @discardableResult
-    public func decrement(by: Int) -> Int {
-        count -= by
-        return count
+    public func decrement(by: Int = -1) -> Int {
+        var c = atomicValue.load(ordering: .sequentiallyConsistent)
+        while !atomicValue.compareExchange(expected: c, desired: c - by, ordering: .sequentiallyConsistent).0 {
+            c = atomicValue.load(ordering: .sequentiallyConsistent)
+        }
+        return c - by
     }
 }
