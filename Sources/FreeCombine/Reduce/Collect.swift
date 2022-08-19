7d60a47eab3bfc3e79860d183310e4bf8f5decac
EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>
2022-08-18 22:31:26 -0400

    Get compiling again
    Signed-off-by: EandJsFilmCrew <789213+rvsrvs@users.noreply.github.com>

--- Sources/FreeCombine/Reduce/Collect.swift
+++ Sources/FreeCombine/Reduce/Collect.swift
@@ -15,7 +15,7 @@ public extension Publisher {
                 }
                 switch r {
                     case .value(let a):
-                        await currentValue.append(a)
+                        try await currentValue.append(a)
                         return .more
                     case let .completion(value):
                         _ = try await downstream(.value(currentValue.value))
